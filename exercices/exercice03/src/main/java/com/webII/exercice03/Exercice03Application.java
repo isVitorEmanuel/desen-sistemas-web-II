b31207f4878b3500d1bfe994fd3586152019b933
Vitor Emanuel <vitoralencar2706@gmail.com>
2024-10-18 16:30:26 -0300

    Feat: initial setup to exercice!

---
+++ exercices/exercice03/src/main/java/com/webII/exercice03/Exercice03Application.java
@@ -0,0 +1,13 @@
+package com.webII.exercice03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercice03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercice03Application.class, args);
+	}
+
+}
---
+++ exercices/exercice03/src/test/java/com/webII/exercice03/Exercice03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webII.exercice03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercice03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
